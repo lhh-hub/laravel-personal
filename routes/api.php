1d159a0c87b01c3bb971e14ce3b60a1f83c1cc02
topit322 <haijun@cecelive.com>
2019-04-24 20:12:33 +0800

    添加拓展

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,38 @@
 
 
 
-$api = app("Dingo\Api\Routing\Router");
+//$api = app("Dingo\Api\Routing\Router");
+//$api->version('v1', function ($api) {
+//    $api->group(["namespace" => "App\Http\Controllers"], function ($api) {
+//
+//        $api->post('login', 'AuthController@login');
+//        $api->post('logout', 'AuthController@logout');
+//        $api->post('refresh', 'AuthController@refresh');
+//        $api->get('index', 'IndexController@index');
+//        $api->get('me', 'AuthController@me');
+//
+//    });
+//});
 
-$api->group(['prefix' => 'auth',"namespace" => "App\Http\Controllers\V1"], function ($api) {
+//Route::version('v1', function ($router) {
+//    Route::group([
+//
+//        'prefix' => 'auth'
+//
+//    ], function ($router) {
+//
+//        Route::post('login', 'AuthController@login');
+//        Route::post('logout', 'AuthController@logout');
+//        Route::post('refresh', 'AuthController@refresh');
+//        Route::post('me', 'AuthController@me');
+//
+//    });
+//});
 
-    $api->post('login', 'AuthController@login');
-    $api->post('logout', 'AuthController@logout');
-    $api->post('refresh', 'AuthController@refresh');
-    $api->post('me', 'AuthController@me');
+$api = app('Dingo\Api\Routing\Router');
 
+//$api->get('index/{id}', 'App\Http\Controllers\IndexController@index');
+$api->version('v1', function ($api) {
+
+    $api->post('login', 'App\Http\Controllers\Api\AuthController@login');
 });
